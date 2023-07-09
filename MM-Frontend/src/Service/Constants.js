ff345f015820c34b9d110ef69fb6467afac87741
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2023-07-08 22:07:13 -0400

    backend api changed

--- MM-Frontend/src/Service/Constants.js
+++ MM-Frontend/src/Service/Constants.js
@@ -2,7 +2,8 @@ export const STRIPE_KEY =
   "pk_live_51N0rSfJpcofebbjLOHNexWo5zEsqtf10Uq6PtPaKA9F93Pf9qGCHf6RGgmQl1B5cffa7eNbRAXPMwAurfUp6msUV002vZyE4MK";
 
 // for cloud testing
-export const BASE_URL = "https://mm-pure-backend-production.up.railway.app/api";
+// export const BASE_URL = "https://mm-pure-backend-production.up.railway.app/api";
+export const BASE_URL = "https://mm-pure-backend-production.onrender.com/api";
 
 
 //// for local testing
